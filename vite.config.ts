e3337a5db4b56bb780f5aac4f29fe4a39080dfd4
Artem-CyberArt <artem2006pax@mail.ru>
2025-04-14 14:40:28 +0300

    asdf

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    open: true,
+  },
+  base: './',
+  build: {
+    outDir: './dist',
+    sourcemap: true,
+  },
+  css: {
+    modules: {
+      localsConvention: 'camelCase',
+    },
+  },
+  resolve: {
+    alias: {
+      '@': './src',
+    },
+  },
+});
